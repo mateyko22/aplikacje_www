ef9e12dfba7d4e39c337b2831d3bec26dec99fae
radeon <radeon@gmail.com>
2023-12-07 14:30:19 +0100

    zad2 / custom queries

--- mysite/schema.py
+++ mysite/schema.py
@@ -1,4 +1,5 @@
 import graphene
+from django.db.models import Q
 from graphene_django import DjangoObjectType
 
 from polls.models import Osoba, Stanowisko
@@ -22,8 +23,11 @@ class Query(graphene.ObjectType):
     all_osobas = graphene.List(OsobaType)
     osoba_by_name = graphene.Field(OsobaType, name=graphene.String(required=True))
     find_osobas_name_by_phrase = graphene.List(OsobaType, substr=graphene.String(required=True))
+    osoba_by_plec = graphene.List(OsobaType, plec=graphene.Int(required=True))
+    osoba_by_data_dodania = graphene.List(OsobaType, data_dodania=graphene.Date(required=True))
+    find_osoba_nazwisko_startswith = graphene.List(OsobaType, substr=graphene.String(required=True))
 
-    def resolve_all_teams(root, info):
+    def resolve_all_stanowiska(root, info):
         return Stanowisko.objects.all()
 
     def resolve_osoba_by_id(root, info, id):
@@ -40,11 +44,19 @@ class Query(graphene.ObjectType):
 
     def resolve_all_osobas(root, info):
         """ zwraca również wszystkie powiązane obiekty team dla tego obiektu Person"""
-        return Osoba.objects.select_related("team").all()
+        return Osoba.objects.all()
 
     def resolve_find_osobas_name_by_phrase(self, info, substr):
-        return Osoba.objects.filter(name__icontains=substr)
+        return Osoba.objects.filter(nazwisko__icontains=substr)
+
+    def resolve_osoba_by_plec(self, info, plec):
+        return Osoba.objects.filter(plec=plec)
+
+    def resolve_osoba_by_data_dodania(self, info, data_dodania):
+        return Osoba.objects.filter(data_dodania__gte=data_dodania)
+
+    def resolve_find_osoba_nazwisko_startswith(self, info, substr):
+        return Osoba.objects.filter(nazwisko__istartswith=substr)
 
 
 schema = graphene.Schema(query=Query)
-
--- polls/models.py
+++ polls/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import date
 
+
 class Stanowisko(models.Model):
     nazwa = models.CharField(max_length=60)
     opis = models.CharField(max_length=60, null=True, blank=True)
